4003d43b71fbe4287ac0c915e57d2c0b477a71b2
BloodBlossom <37664359+BloodBlossom@users.noreply.github.com>
2019-01-18 16:35:47 +0800

    再次提交

--- app/src/main/java/com/blossom/helloworld/MainActivity.java
+++ app/src/main/java/com/blossom/helloworld/MainActivity.java
@@ -20,7 +20,6 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 tv1.setText("Good Bye!");
-                ;
             }
         });
 
