ff10def5dc5446974fde898dd95df7a019e6801c
BloodBlossom <37664359+BloodBlossom@users.noreply.github.com>
2019-01-19 00:42:15 +0800

    周六作业

--- app/src/main/java/com/blossom/helloworld/MainActivity.java
+++ app/src/main/java/com/blossom/helloworld/MainActivity.java
@@ -4,29 +4,55 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Switch;
 import android.widget.TextView;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
+    public int num = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btn1 = findViewById(R.id.btn1);
-        final TextView tv1 = findViewById(R.id.tv1);
+        Button btn = findViewById(R.id.button1);
+        Button btn2 = findViewById(R.id.button2);
+        Switch switch1 = findViewById(R.id.switch1);
+        final TextView textView = findViewById(R.id.textView2);
 
-        btn1.setOnClickListener(new View.OnClickListener() {
+
+        btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                tv1.setText("Good Bye!");
-                ;
-
-
+                EditText editText = findViewById(R.id.editText1);
+                textView.setText(editText.getText());
             }
         });
 
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               TextView text1 = findViewById(R.id.textView5);
+               num = num+1;
+               text1.setText(Integer.toString(num));
+            }
+        });
 
-
+        switch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                TextView text2 = findViewById(R.id.textView6);
+                if(isChecked){
+                    text2.setText(R.string.anonymous);
+                }
+                else{
+                    text2.setText(R.string.myname);
+                }
+            }
+        });
     }
 }
